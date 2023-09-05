7864ba5f45c7f074993c3a2412ef9f3825cde900
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-09-05 12:42:45 +0400

    Add login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,44 @@
-import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="bg-white text-blue-600 p-5 rounded-lg max-w-[90%] w-[500px] shadow-lg">
       <h1 className="text-2xl font-bold mb-5">Chat App Login</h1>
-      <form className="flex flex-col">
+      <form onSubmit={handleSubmit} className="flex flex-col">
         <label className="mb-3">Email</label>
         <input
           className="border border-gray-400 p-2 rounded-lg mb-3"
           type="email"
           placeholder="your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label className="mb-3">Password</label>
         <input
           className="border border-gray-400 p-2 rounded-lg mb-3"
           type="password"
           placeholder="your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className="bg-blue-600 text-white py-2 rounded-lg">
           Login
